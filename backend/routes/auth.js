6e72b0aadf4dafe64484584b952a712ac754d51b
Andrew <bohdan.andrii@gmail.com>
2020-11-15 18:51:21 -0500

    Add getUser route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,47 +1,71 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const dbWrapper = require('../js/db-wrapper');
+const url = require("url");
+const querystring = require("querystring");
 
-router.post('/login', (req, res, next) => {
-	const email = req.body.email;
-	const token = req.body.token;
-	dbWrapper.getUsersByEmail(email).then((user) => {
-		console.log(user);
-		dbWrapper.updateUser(user._id, {
-			token: token,
-		});
-	});
+const dbWrapper = require("../js/db-wrapper");
 
-	res.json({
-		success: true,
-	});
+router.post("/login", (req, res, next) => {
+    const email = req.body.email;
+    const token = req.body.token;
+    dbWrapper.getUsersByEmail(email).then((user) => {
+        console.log(user);
+        dbWrapper.updateUser(user._id, {
+            token: token,
+        });
+    });
+
+    res.json({
+        success: true,
+    });
 });
 
-router.post('/logout', (req, res, next) => {
-	const token = req.body.token;
-	dbWrapper.getUserByToken(token).then((user) => {
-		dbWrapper.updateUser(user._id, {
-			token: '',
-		});
-	});
+router.post("/logout", (req, res, next) => {
+    const token = req.body.token;
+    dbWrapper.getUserByToken(token).then((user) => {
+        dbWrapper.updateUser(user._id, {
+            token: "",
+        });
+    });
 
-	res.json({
-		success: true,
-	});
+    res.json({
+        success: true,
+    });
 });
 
-router.post('/register', (req, res, next) => {
-	const token = req.body.token;
-	const email = req.body.email;
-	const name = req.body.name;
+router.post("/register", (req, res, next) => {
+    const token = req.body.token;
+    const email = req.body.email;
+    const name = req.body.name;
 
-	dbWrapper.insertUser({ email, token, name, role: 's' }).then((user) => {
-		res.json({
-			success: true,
-			user: user,
-		});
-	});
+    dbWrapper.insertUser({ email, token, name, role: "s" }).then((user) => {
+        res.json({
+            success: true,
+            user: user,
+        });
+    });
+});
+
+router.get("/getUser", (req, res, next) => {
+    let parsedUrl = url.parse(req.url);
+    let parsedQs = querystring.parse(parsedUrl.query);
+
+    let token = parsedQs["token"];
+
+    if (!token) {
+        res.json({
+            success: false,
+            message: "Token was not supplied",
+        });
+    }
+
+    dbWrapper.getUserByToken(token).then((user) => {
+        res.json({
+            success: true,
+            user: user,
+        });
+    });
 });
 
 module.exports = router;
