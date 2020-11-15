6e7c282140a1b4a554c219dc339714c5087f524b
Andrew <bohdan.andrii@gmail.com>
2020-11-15 04:29:29 -0500

    Store token in database on login/logout

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-const cookieParser = require('cookie-parser');
+const dbWrapper = require('../js/db-wrapper');
 
 router.post('/login', (req, res, next) => {
-	console.log(req.body.token);
+	const email = req.body.email;
+	const token = req.body.token;
+	dbWrapper.getUsersByEmail(email).then((user) => {
+		console.log(user);
+		dbWrapper.updateUser(user[0]._id, {
+			token: token,
+		});
+	});
 
 	res.json({
 		success: true,
@@ -12,7 +19,13 @@ router.post('/login', (req, res, next) => {
 });
 
 router.post('/logout', (req, res, next) => {
-	console.log(req.body.token);
+	const token = req.body.token;
+	dbWrapper.getUsersByToken(token).then((user) => {
+		console.log(user);
+		dbWrapper.updateUser(user[0]._id, {
+			token: '',
+		});
+	});
 
 	res.json({
 		success: true,
