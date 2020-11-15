961634f477045ae3f47049b07238d021bd448059
Andrew <bohdan.andrii@gmail.com>
2020-11-14 19:30:04 -0500

    Add more test routes

--- backend/routes/roles/teacher.js
+++ backend/routes/roles/teacher.js
@@ -1,24 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const url = require('url');
-const querystring = require('querystring');
+const url = require("url");
+const querystring = require("querystring");
 
 /* GET home page. */
-router.post('/changePLink', function (req, res, next) {
-	//TODO: update the record in the database
+router.post("/changePLink", function (req, res, next) {
+    //TODO: update the record in the database
 
-	res.json({
-		message: 'New code: ' + req.body.code,
-		success: true,
-	});
+    res.json({
+        message: "New code: " + req.body.code,
+        success: true,
+    });
 });
 
-router.post('/addCourse', (req, res, next) => {
-	res.json({
-		message: 'Add course: ' + req.body.name,
-		success: true,
-	});
+router.post("/addCourse", (req, res, next) => {
+    res.json({
+        message: "Add course: " + req.body.name,
+        success: true,
+    });
+});
+
+router.get("/getPLink", (req, res, next) => {
+    let parsedUrl = url.parse(req.url);
+    let parsedQs = querystring.parse(parsedUrl.query);
+
+    const id = parsedQs["id"];
+
+    res.json({
+        success: true,
+        link: "/assets/testdoc.pdf",
+    });
 });
 
 module.exports = router;
