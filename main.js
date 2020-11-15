16229d253e7392bdec9fe07018fbe7a787d43717
amuleek simak <amuleeksimak@gmail.com>
2020-11-14 22:10:36 -0500

    tried to implement pdf.js, did not work, need to fix

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-function getSlideNum() {
-    
-    
+
+const url = 'images/recursion.pdf';
+
+let pdfDoc = null,
+    currentPage = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+const scale = 1.5,
+    canvas = document.querySelector('#pdf_render'),
+    ctx = canvas.getContext('2d');
+
+//Render the page
+const renderPage = num => {
+
 }
+
+//Get document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+
+    pdfDoc = pdfDoc_;
+    console.log(pdfDoc);
+
+    
+});
