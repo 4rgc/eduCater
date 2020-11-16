9781dcd9208f0a06fa40cfcac2179d252151b4a2
Andrew <bohdan.andrii@gmail.com>
2020-11-15 21:44:01 -0500

    Fix myCourses page

--- backend/public/javascripts/myProfile.js
+++ backend/public/javascripts/myProfile.js
@@ -2,7 +2,9 @@ function editProfile() {
     var unameInputBox = document.getElementById("uname");
     var myButton = document.getElementById("edit");
     unameInputBox.disabled = !unameInputBox.disabled;
-    myButton.innerHTML = unameInputBox.disabled ? "Edit Profile" : "Save changes";
+    myButton.innerHTML = unameInputBox.disabled
+        ? "Edit Profile"
+        : "Save changes";
     // Something something changes get saved back to database
 }
 
@@ -13,3 +15,16 @@ function changepfp() {
 function getUser() {
     // TODO: gets user JSON object and displays proper information about them
 }
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        user.getIdToken().then(function (idToken) {
+            httpPostAsync(
+                "/login",
+                `token=${idToken}&email=${user.email}`,
+                () => {}
+            );
+        });
+        console.log("Welcome, " + user.email);
+    }
+});
